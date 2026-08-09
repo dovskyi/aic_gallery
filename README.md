fc1212096e574a0477cdf7088e57474157de7479
Mykyta Dombrovskyi <mykyta.dombr@gmail.com>
2026-08-08 22:45:00 -0400

    Added dynamic rearrange

--- README.md
+++ README.md
@@ -1,47 +1,94 @@
+
 # AIC GALLERY
 
 I loved how AICs Website Gallery looks. Equal columns, image is at native ratio, no empty space. Layout is not chaotic, but at the same time not overly linear. Very elegant.
 
-Turns out you can't do that with css, so I wrote a small js script that achieves this behaviour.
+Turns out you can't do that with vanilla css, so I wrote a small JS script that achieves this behaviour.
 Gallery is primarily for organizing actual elements, CSS is on you.
 
 *example with custom css*
 ![gallery_showcase_css](img/gallery_showcase_css.png)
 *how raw gallery looks*
 ![gallery_showcase_raw](img/gallery_showcase_raw.png)
+*Also check it out at [dovskyi.org/gallery](https://dovskyi.org/src/gallery.php)!*
 
-## What do you need?
+### What do you need?
 
-* A div with unique id that will contain the gallery
-* An array of whatever you want to output
-* Height and Width in px for **every** image in array
-* Manually link the aic_gallery.css file
+* A div with unique ID that will contain the gallery
+* Array of images and/or misc data
+* Height and Width for every image if used, in px
+
+**gallery supports multiple instances per page and block rearrangement**
 
 ## USAGE
 Gallery is initialized using `gallery_init()` function. There are some arguments:
 ```
 gallery_init(array, container_id, cols, template, load_delay);
+```
+1. array: your array with items.
+2. container_id: id of a container where the gallery will be contained.
+3. cols: amount of columns you want to have (3-4 is good).
+4. template: string of HTML elements for a single entry.
+5. load_delay [OPTIONAL]: it can look cool! (set in milliseconds).
 
-array: your array with items
-container_id: id of a container where the gallery will be contained
-cols: amount of columns you want to have (3-4 is good)
-template: string of HTML elements for a single entry
+### Rearrange blocks function:
+Blocks can be rearranged in the same container to a different number of columns. Note, that new number of columns can only be smaller or equal to number of columns in initialization. So if you set cols=3 in gallery_init(), you can only rearrange to 1-3 columns. If you want to rearrange to a greater number as a workaround, you can first initialize it to that number & then rearrange to your desired smaller number.
 
-load_delay[OPTIONAL]: it can look cool! (set in milliseconds)
+Usage for rearrange function:
+```
+aic_rearrange(new cols, container_id);
+
+new cols: new number of columns. Must be <= than columns in initialization
+container_id: string name of the gallery you want to rearrange
 ```
 
+This was added for my own usecase, so I am sorry about not being able to rearrange to more columns--i don't need that capability.
+
+### Step 1: Add CSS and JS files
+```
+<link rel='stylesheet' href="/path/to/aic_gallery.css">
+<script src="/path/to/aic_gallery.js"></script>
+```
+### Step 2: Add container div
+```
+<div id="whatever_id" class="whatever_class"></div>
+</div>
+```
+### Step 3: Run init function
+```
+<script>
+const arr = <?php echo json_encode($image_array) ?>;
+const container_id = "whatever_id";
+const cols = 3;
+
+gallery_init(arr, container_id, cols, function(entry){
+return 
+`
+	<div class="entry_container">
+	  <img 
+	  src="${entry.image_path}" 
+	  data-width=${entry.width}
+	  data-height=${entry.height}
+	  >
+	  <span class="entry_title"><b>${entry.image_title}</b></span>
+	  <span class="entry_date">${entry.image_date}</span>
+	</div>
+`
+;});
+</script>
+```
+## Required Syntax & limitations
+* \<img\> element **must** have data-width and data-height as INT.
+* Entry must have some sort of item wrapper div, e.g.:
+```
+<div class="required_wrapper">
+	//whatever you need for single item
+</div>
+```
+* **Only item wrapper can have horizontal margins/padding/borders**. Vertical is fine for any internal element. If you want distance between columns, add margins to the item wrapper. There is usually no need to edit library css, unless you are doing something very specific.
+
 ### Initialization Examples:
-
-**You MUST add data-width and data-height as INT to \<img\> tag, see below in example**
-
-*Note, AIC website itself uses pre-calculated img dimensions, so all fair*
-
-This gallery would:
-* take in an associative PHP array, 
-* bind to a container with id "gallery_container"
-* have 3 columns
-* output a specific DOM structure for each element. 
-
+Full example from above
 ```
 <link rel='stylesheet' href="/css/aic_gallery.css">
 
@@ -82,6 +129,7 @@ Simpler example with just an image and a cosmetic 250ms delay:
 const arr = [["/path/to/img1", 480, 280],["/path/to/img2", 1080, 2042]];
 const container_id = "some_other_container";
 const cols = 3;
+const delay_ms = 250;
 
 gallery_init(arr, container_id, cols, function(entry){
     return 
@@ -90,7 +138,7 @@ gallery_init(arr, container_id, cols, function(entry){
         data-width=${entry[1]}
         data-height=${entry[2]}
         >`
-    ;}, 250);
+    ;}, delay_ms);
 </script>
 
 <div id="some_other_container" class="gallery_container"></div>
@@ -99,6 +147,4 @@ gallery_init(arr, container_id, cols, function(entry){
 ```
 
 ## CSS and function(entry)
-As you can see, you can enter anything into return as a string, and that will be the structure for every item in the gallery. This means that all you have to do to change CSS is create your own wrappers and rules for them. For example, in init function #1, "entry_container" is a custom class with its own css rules. Want a description for image? Add a div, add a variable, write css rule, and that is it. 
-
-Your entry must have a div that wraps the entire item, and **all horizontal margins/padding/borders should only be applied to that div**. Gallery does make one for its own purposes, but it calculates margins only for 1st child, which would be your wrapper div. If you apply horizontal shift to internal elements, it will break the layout.
+As you can see, you can enter anything into return as a string, and that will be the structure for every item in the gallery. This means that all you have to do to change CSS is create your own wrappers and rules for them. For example, in init function #1, "entry_container" is a custom class with its own css rules. Want a description for image? Add a div, add a variable, write a css rule, and that is it. 
--- aic_gallery.js
+++ aic_gallery.js
@@ -3,15 +3,15 @@
  * 2026-07-12
  * GPL-3.0 license
  */
-
-let _gallID = 0; //global gallery ID for every instance
+let _gallID = 0; //global gallery ID
+var _instances = []; //data for all galleries on page
 const _delay = ms => new Promise(res => setTimeout(res, ms));
 
-function _smallestInArray(arr_col) {
+function _smallestInArray(arr_col, cols) {
         let a = Number.MAX_SAFE_INTEGER;
         let indx = 0;
-        for (let i=0; i<arr_col.length; i++){
-                let h = arr_col[i].scrollHeight;
+        for (let i=0; i<cols; i++){
+                let h = arr_col[i].offsetHeight;
                 if (a > h) {
                         a = h;
                         indx = i;
@@ -21,32 +21,36 @@ function _smallestInArray(arr_col) {
 }
 
 function _get_margins(block) {
-        //here block is user's entry container
+        //here block is user's entry container, not class wrapper
         let spacing = 0;
         let style = window.getComputedStyle(block);
         spacing  = (parseFloat(style.marginLeft) || 0) +
-                   (parseFloat(style.marginRight) || 0) +
-                   (parseFloat(style.paddingLeft) || 0) +
-                   (parseFloat(style.paddingRight) || 0) +
-                   (parseFloat(style.borderLeftWidth) || 0) +
-                   (parseFloat(style.borderRightWidth) || 0);
+                (parseFloat(style.marginRight) || 0) +
+                (parseFloat(style.paddingLeft) || 0) +
+                (parseFloat(style.paddingRight) || 0) +
+                (parseFloat(style.borderLeftWidth) || 0) +
+                (parseFloat(style.borderRightWidth) || 0);
 
         return spacing;
 }
+
 
 function gallery_init(arr, gallery_container, cols, template, load_delay){
 
         const timeout = (load_delay === undefined)? false: true;
 
-        window.addEventListener('load', async function(event) {
-                const gallery_wrapper = document.getElementById("gallery_container"); //user's container for gallery
+        window.addEventListener('DOMContentLoaded', async function(event) {
+                const gallery_wrapper = document.getElementById(gallery_container); //user's container for gallery
                 _gallID++;
+                //add instance to global array
+                _instances[gallery_container] = {arr_col: [],
+                                                blocks: []};
+
+                let arr_col = _instances[gallery_container].arr_col;
+                let blocks = _instances[gallery_container].blocks;
 
                 if (cols < 1) {
                         window.alert("Gallery columns can't be <1");
-                }
-                if (arr.length < 1) {
-                        return;
                 }
                 gallery_wrapper.insertAdjacentHTML("beforeend", `<div id='gallery_cont${_gallID}' class='gallery_cont'></div>`);
                 gallery_wrapper.insertAdjacentHTML("beforeend", `<div id='temp_holder${_gallID}' style='visibility: hidden; position:absolute;'></div>`);
@@ -54,13 +58,15 @@ function gallery_init(arr, gallery_container, cols, template, load_delay){
                 const container = document.getElementById(`gallery_cont${_gallID}`);
 
 
-                let arr_col = [];
 
                 for (let i=0; i<cols; i++) {
                         //partition the container into n columns, write columns into array
                         let col = `<div id="col_${i}${_gallID}" class="gallery_col"><div id="wrapper_col_${i}${_gallID}"></div></div>`;
                         container.insertAdjacentHTML("beforeend", col);
                         arr_col.push(document.getElementById(`wrapper_col_${i}${_gallID}`));
+                }
+                if (arr.length < 1) {
+                        return;
                 }
 
                 const col_width = arr_col[0].offsetWidth;
@@ -73,30 +79,32 @@ function gallery_init(arr, gallery_container, cols, template, load_delay){
                 //now that we have all blocks as objects, write them into array
                 //(has to be a static array, because children method itself
                 //actually returns an HTMLCollection)
-                let blocks = Array.from(temp.children);
+                blocks = Array.from(temp.children);
                 let margin_spacing = _get_margins(blocks[0].firstElementChild);
                 let scale_width = col_width - margin_spacing;
 
                 for (const block of blocks) {
                         //create parent div for image
                         const img = block.querySelector("img");
-                        const img_wrap = document.createElement("div");
+                        if (img) {
+                                const img_wrap = document.createElement("div");
 
-                        img.before(img_wrap);
-                        img_wrap.appendChild(img);
+                                img.before(img_wrap);
+                                img_wrap.appendChild(img);
 
-                        const img_width = img.dataset.width;
-                        const img_height = img.dataset.height;
+                                const img_width = img.dataset.width;
+                                const img_height = img.dataset.height;
 
-                        const r = img_width/img_height;
-                        //force wrapper div into img dimensions
-                        img_wrap.style.display = "block";
-                        img_wrap.style.width = scale_width + "px";
-                        img_wrap.style.height = scale_width/r + "px";
+                                const r = img_width/img_height;
+                                //force wrapper div into img dimensions
+                                img_wrap.style.display = "block";
+                                img_wrap.style.width = scale_width + "px";
+                                img_wrap.style.height = scale_width/r + "px";
+                        }
 
                         //append block to shortest column
                         //appendChild removes duplicates automatically
-                        const curr_index = _smallestInArray(arr_col);
+                        const curr_index = _smallestInArray(arr_col, cols);
                         arr_col[curr_index].appendChild(block);
 
                         if (timeout) {
@@ -106,4 +114,42 @@ function gallery_init(arr, gallery_container, cols, template, load_delay){
                 }
                 temp.remove();
         });
+}
+
+function aic_rearrange(cols, gallery_container){
+        const arr_col = _instances[gallery_container].arr_col;
+        const blocks = _instances[gallery_container].blocks;
+
+        const maxCols = arr_col.length;
+        const container = document.getElementById(gallery_container);
+
+        if (cols > maxCols || cols < 1) {
+                window.alert("Rearrangement can't be smaller than 1 OR greater than initialization columns");
+                return;
+        }
+
+        //manually set current height so view doesn't jump when all blocks are removed
+        container.style.minHeight = container.scrollHeight + "px";
+
+        //remove all blocks from container
+        for (const block of blocks) {
+                block.remove();
+        }
+
+        //toggle visibility for columns
+        for (let i=0; i<cols; i++) {
+                arr_col[i].style.display = '';
+        }
+        for (let i=maxCols; i>cols; i--) {
+                arr_col[i-1].style.display = 'none';
+        }
+
+        //reappend blocks to visible columns
+        for (const block of blocks) {
+                const curr_index = _smallestInArray(arr_col, cols);
+                arr_col[curr_index].appendChild(block);
+        }
+
+        //remove manual height
+        container.style.minHeight = '';
 }
